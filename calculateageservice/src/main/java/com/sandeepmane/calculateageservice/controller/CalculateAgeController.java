12caaa801d8a70c9d037694f299c36d1d12e02f1
Sandeep Mane <sandeep7000@gmail.com>
2020-06-06 13:46:58 -0400

    Retrieving JSON as response and stop registering calculateservice with Eureka server

--- calculateageservice/src/main/java/com/sandeepmane/calculateageservice/controller/CalculateAgeController.java
+++ calculateageservice/src/main/java/com/sandeepmane/calculateageservice/controller/CalculateAgeController.java
@@ -3,8 +3,10 @@ package com.sandeepmane.calculateageservice.controller;
 import com.sandeepmane.calculateageservice.service.CalculateAgeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,7 @@ public class CalculateAgeController {
     @Autowired
     CalculateAgeService calculateAgeService;
 
-    @RequestMapping("/calculate")
-    @PostMapping
+    @RequestMapping(path = "/calculate", produces = MediaType.APPLICATION_JSON_VALUE)
     public int calculateAge(@RequestParam(name = "name") String name)
     {
         int age;
